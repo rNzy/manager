526592d83878ccc2959e2c3e1bfa9b08cf7f070d
Frederic Espiau <frederic.espiau@corp.ovh.com>
2020-04-09 15:15:59 +0200

    refactor(ip): migrate dashboard.order to component
    Signed-off-by: Frederic Espiau <frederic.espiau@corp.ovh.com>

---
+++ packages/manager/apps/dedicated/client/app/ip/dashboard/order/order.constant.js
@@ -0,0 +1,46 @@
+export const FETCH_PRICE_MAX_TRIES = 5;
+
+export const IP_LOCATION_GROUPS = [
+  { labels: ['APAC/CANADA', 'CANADA - ASIA'], countries: ['au', 'ca', 'sg'] },
+  {
+    labels: ['EUROPE'],
+    countries: [
+      'be',
+      'cz',
+      'de',
+      'es',
+      'fi',
+      'fr',
+      'gb',
+      'ie',
+      'it',
+      'lt',
+      'nl',
+      'pl',
+      'pt',
+      'uk',
+    ],
+  },
+  { labels: ['USA'], countries: ['us'] },
+];
+
+export const PRODUCT_TYPES = {
+  dedicatedServer: {
+    apiTypeName: 'SERVER',
+    typeName: 'DEDICATED',
+  },
+  privateCloud: {
+    apiTypeName: 'DEDICATED_CLOUD',
+    typeName: 'PRIVATE_CLOUD',
+  },
+  vps: {
+    apiTypeName: 'VPS',
+    typeName: 'VPS',
+  },
+};
+
+export default {
+  FETCH_PRICE_MAX_TRIES,
+  IP_LOCATION_GROUPS,
+  PRODUCT_TYPES,
+};
