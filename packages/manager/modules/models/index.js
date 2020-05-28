c3da9f4574fc1d597b31337744cde741b27a0536
Marie JONES <marie.jones@corp.ovh.com>
2020-05-28 16:22:56 +0200

    refactor(support-level): move class to model module
    Signed-off-by: Marie JONES <marie.jones@corp.ovh.com>

--- packages/manager/modules/models/index.js
+++ packages/manager/modules/models/index.js
@@ -2,9 +2,26 @@ import BillingService from './BillingService/BillingService.class';
 import Certificate from './certificate/Certificate.class';
 import Price from './Price/Price.class';
 import Service from './service/Service.class';
+import SupportLevel from './support/level/SupportLevel.class';
 import Ticket from './support/ticket/ticket.class';
 import User from './user/User.class';
 
-export { BillingService, Certificate, Price, Service, Ticket, User };
+export {
+  BillingService,
+  Certificate,
+  Price,
+  Service,
+  SupportLevel,
+  Ticket,
+  User,
+};
 
-export default { BillingService, Certificate, Price, Service, Ticket, User };
+export default {
+  BillingService,
+  Certificate,
+  Price,
+  Service,
+  SupportLevel,
+  Ticket,
+  User,
+};
